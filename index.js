4dec0b2080d40dc8d7512d3302fb99ba5ef668e2
Jaouad E <jaouad.elmoussaoui@gmail.com>
2015-10-14 16:17:37 +0100

    Run karma only on watch or tdd

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 var karma = require('karma').server;
 var _ = require('underscore');
+var gutils = require('gulp-util');
 
 var task = elixir.Task;
 
 elixir.extend('karma', function(args) {
+    if (! _.contains(['watch', 'tdd'], gutils.env._)) return;
+
     var defaults = {
         configFile: process.cwd()+'/karma.conf.js',
         jsDir: ['resources/assets/js/**/*.js', 'resources/assets/coffee/**/*.coffee']
@@ -16,11 +19,10 @@ elixir.extend('karma', function(args) {
     var isStarted = false;
 
     new task('karma', function() {
-        if( ! isStarted) {
+        if( ! isStarted && (_.contains(['watch', 'tdd'], gutils.env._))) {
             karma.start(options);
             isStarted = true;
         }
     }).watch(options.jsDir, 'tdd');
-
 });
 
